ab057fd9532b2dfc862154845596e3f2dd783ef1
Yogesh Dande <dandeyogesh25@gmail.com>
2021-08-17 12:02:27 +0530

    zone added

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,6 +1,6 @@
 
 
-CREATE TABLE public.review_rating (
+CREATE TABLE review_rating (
 	review_id int8 NOT NULL,
 	place_id int8 NULL,
 	review_description varchar(255) NULL,
@@ -11,6 +11,6 @@ CREATE TABLE public.review_rating (
 	CONSTRAINT review_rating_pkey PRIMARY KEY (review_id)
 );
 
-INSERT INTO public.review_rating
+INSERT INTO review_rating
 ( place_id, review_description, review_star, user_mobile)
 VALUES( 1, 'Good place', 4.0, '7385787937');
