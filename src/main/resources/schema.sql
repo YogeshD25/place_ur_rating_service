be8c0a7e783d2c6784a9e06a4bd4d67bd4f2038e
Yogesh Dande <dandeyogesh25@gmail.com>
2021-08-17 12:19:07 +0530

    zone added

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,14 +1,12 @@
 
 
 CREATE TABLE review_rating (
-	review_id int8 NOT NULL,
+    review_id SERIAL PRIMARY KEY,
 	place_id int8 NULL,
 	review_description varchar(255) NULL,
 	review_star float4 NULL,
-	created_at DATETIME NOT NULL
-                    DEFAULT CURRENT_TIMESTAMP,
-	user_mobile varchar(255) NULL,
-	CONSTRAINT review_rating_pkey PRIMARY KEY (review_id)
+	created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+	user_mobile varchar(255) NULL
 );
 
 INSERT INTO review_rating
